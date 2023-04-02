b816eda93d31de4af2cb4c5e8d4c1492c323e1e8
Rasmus Eskola <fruitiex@gmail.com>
2023-04-02 22:09:25 +0300

    Add fluxcd to nix shell, pin nixpkgs version

--- default.nix
+++ default.nix
@@ -1,9 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/143b829e58f25437e61d1f766fc9644fd9139ea0.tar.gz") {}
+}:
 pkgs.mkShell {
   name = "homelab";
   buildInputs = with pkgs; [
     _1password
     gnupg
     sops
+    fluxcd
   ];
 }
