25bcacc08dee2c5529be36320a1475d6c2528690
Rasmus Lövegren <rasmus@lovegren.fi>
2025-10-03 13:16:55 +0300

    chore: bump immich

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/893757b4674d2653daf6d0cab18615d2570a29cd.tar.gz") {}
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/356294f35290bf10442bfc60408cbe5ccde5a202.tar.gz") {}
 }:
 pkgs.mkShell {
   name = "homelab";
---
+++ regen-configs.sh
@@ -0,0 +1,1 @@
+talosctl gen config homelab-cluster https://192.168.10.97:6443 --with-secrets secrets.yaml --output-dir _out --force
